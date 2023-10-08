8dd6edbf09e538fab2f72fcdd0a7ff2214aa7977
Pranav Raju <120582474+pranavraju1@users.noreply.github.com>
2023-10-08 11:11:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+let url = document.getElementById("url");
+let submit = document.getElementById("button");
+let name = document.getElementById("name");
+let year = document.getElementById("year");
+submit.addEventListener("click",(event)=>{
+    event.preventDefault();
+    let NAME = name.value;
+    let YEAR = year.value;
+    if(NAME == "" && YEAR != ""){
+        url.innerText="https://localhost:8080/?year="+`${YEAR}`;    
+    }else if(NAME != "" && YEAR == ""){
+        url.innerText="https://localhost:8080/?name="+`${NAME}`;
+    }else{
+        url.innerText="https://localhost:8080/?name="+`${NAME}`+"&year="+`${YEAR}`;
+    }
+});
